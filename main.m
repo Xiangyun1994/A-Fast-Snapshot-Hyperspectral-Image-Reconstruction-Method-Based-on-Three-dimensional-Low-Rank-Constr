861e4b9d3a8a40c02483e1fc3344abad090047bf
Xiangyun1994 <57108550+Xiangyun1994@users.noreply.github.com>
2021-03-25 19:51:45 +0800

    Add files via upload

---
+++ main.m
@@ -0,0 +1,29 @@
+clc
+clear all
+close all
+addpath('CTISfunction')
+addpath('data');
+
+load('pandadata.mat');
+load('Mu.mat');
+load('Mu1.mat');
+load('calipar.mat');
+
+%% load Mask
+Mu2 = repmat(Mu,[1,1,par.k]);
+par.Mu1 = Mu1;
+par.Mu2 = Mu2;
+
+%% input
+par.data = g;
+
+par.iter = 3;
+fctEM = CodeEMf1(par);
+
+par.iter = 3;
+fctCFW = CodeLRf(par);
+
+par.iter = 3;
+[fctCPat]=CodePatf(par);
+
+
